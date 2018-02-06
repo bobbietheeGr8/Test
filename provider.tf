e01c10f002017885c1b9d07fccff605d018fedb3
Bobbie Duh <CamilleT89@aol.com>
2018-02-05 23:21:17 -0600

    Modified cred path

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
+# Define Provider
 provider "aws" {
     version                 = "~> 1.8s"
     region                  = "us-east-1"
-    shared_credentials_file = "$HOME/.aws/credentials"
+    shared_credentials_file = "~/.aws/credentials"
 }
